3a483a98be2f2722b08fcb048be0b2cd6d49a549
Adriaan Beiertz <adriaanbd@gmail.com>
2020-11-01 23:49:58 -0500

    added examples if name is main

--- python/isitu.py
+++ python/isitu.py
@@ -1,17 +1,20 @@
+from json.decoder import JSONDecodeError
 import boto3
 from typing import List, Union
 import json
 
 
-def all_public_amis_by_path_from_ssm():
+def all_public_amis_by_path_from_ssm(path: str=None):
     """
     The AWS CLI equivalent of:
     aws ssm get-parameters-by-path
     --path "/aws/service/ami-amazon-linux-latest"
     --region us-east-1
     """
+    if path is None:
+        path = "/aws/service/ami-amazon-linux-latest"
     ssm = boto3.client('ssm')
-    response = ssm.get_parameters_by_path()
+    response = ssm.get_parameters_by_path(Path=path)
     return response
 
 def get_ami_data(names: Union[str, List[str]]=None) -> List[dict]:
@@ -26,13 +29,19 @@ def get_ami_data(names: Union[str, List[str]]=None) -> List[dict]:
     metadata: dict = response['ResponseMetadata']
     if metadata['HTTPStatusCode'] == 200:
         params: List[dict] = response['Parameters']
-        amis_data: List[dict] = [json.loads(p['Value']) for p in params]
+        try:
+            amis_data: List[dict] = [json.loads(p['Value']) for p in params]
+        except JSONDecodeError:
+            amis_data: List[dict] = [p['Value'] for p in params]
         return amis_data
     return
 
 def get_ami_id(names: Union[str, List[str]]=None) -> List[str]:
     amis_data = get_ami_data(names)
-    ids = [value.get('image_id') for value in amis_data]
+    if isinstance(amis_data[0], dict):
+        ids = [value.get('image_id') for value in amis_data]
+    else:
+        ids = amis_data
     return ids
 
 def get_all_region_names() -> List[str]:
@@ -52,5 +61,10 @@ def create_key_pair(key_name: str) -> dict:
     pem: str = response['KeyMaterial']
     return pem
 
-ssm_ami = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended"
-print(get_ami_id([ssm_ami]))
+if  __name__ == '__main__':
+    print('Examples:\n')
+    ssm_ami = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended"
+    print('ECS Optimized AMI id', get_ami_id([ssm_ami]), sep='\n')
+    print('Linux 2 AMI ID', get_ami_id(), sep='\n')
+    print('All region names:', get_all_region_names(), sep='\n')
+    print('All public AMIS by path', all_public_amis_by_path_from_ssm(), sep='\n')
